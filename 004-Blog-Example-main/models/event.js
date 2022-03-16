3b12b68b120c393d6d796c5ab00592c638b1535b
alice <liqi.chen@student.h-is.com>
2022-03-16 12:26:00 +0100

    a

---
+++ 004-Blog-Example-main/models/event.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const eventSchema = new mongoose.Schema({
+  title: String,
+  date: String,
+  content: String,
+  thumbnail: String,
+  
+})
+
+
+mongoose.model('events', eventSchema)
